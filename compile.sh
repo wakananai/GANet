f6393a9acb566524278e9ba92926d27122c075ca
wakananai <wakananai@gmail.com>
2020-01-15 20:24:00 +0800

    assign the compile script to use cuda-10

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #export LD_LIBRARY_PATH="/home/feihu/anaconda3/lib:$LD_LIBRARY_PATH"
 #export LD_INCLUDE_PATH="/home/feihu/anaconda3/include:$LD_INCLUDE_PATH"
-#export CUDA_HOME="/usr/local/cuda-10.0"
+export CUDA_HOME="/usr/local/cuda-10.0"
 #export PATH="/home/feihu/anaconda3/bin:/usr/local/cuda-10.0/bin:$PATH"
 #export CPATH="/usr/local/cuda-10.0/include"
 #export CUDNN_INCLUDE_DIR="/usr/local/cuda-10.0/include"
